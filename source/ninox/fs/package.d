b4365b11eb1614ba9c58a409ab0228f211920aa8
Mai-Lapyst <info@lapyst.by>
2023-07-31 01:09:43 +0200

    Add doc comments

--- source/ninox/fs/package.d
+++ source/ninox/fs/package.d
@@ -30,29 +30,46 @@ import std.path : buildPath, buildNormalizedPath, absolutePath;
 import std.file : StdDirEntry = DirEntry;
 import std.stdio : StdIoFile = File;
 
+/// Baseclass for all exceptions of this package
 class NinoxFsException : Exception {
     this(string msg, Throwable nextInChain = null) pure nothrow @nogc @safe {
         super(msg, __FILE__, __LINE__, nextInChain);
     }
 }
 
+/// Exception for security issues like path traversal issues for sub-filesystems
 class NinoxFsSecurityException : NinoxFsException {
     this(string msg, Throwable nextInChain = null) pure nothrow @nogc @safe {
         super(msg, nextInChain);
     }
 }
 
+/// Exception for when a file or directory could not be found
 class NinoxFsNotFoundException : NinoxFsException {
     this(string msg, Throwable nextInChain = null) pure nothrow @nogc @safe {
         super(msg, nextInChain);
     }
 }
 
+/// Basic interface to interact with a file
 interface File {
+    /**
+     * Reads a chunk of data of the specific size and returns it. Runs syncronously.
+     * 
+     * Params:
+     *   size = the size to read
+     *
+     * Returns: a buffer with the requested data
+     */
     void[] read(int size);
+
+    /**
+     * Closes the current file and disallowing any further interaction with it.
+     */
     void close();
 }
 
+/// Class to represent an File made from a byte array
 class ByteArrayFile : File {
     private {
         void[] data;
@@ -78,13 +95,22 @@ class ByteArrayFile : File {
     }
 }
 
+/// Enum to represent the kind of a file or path
 enum FileKind {
+    /// Kind not known; should be treated as an error
     Unknown,
+
+    /// Directory / Folder
     Dir,
+
+    /// Regular file
     File,
+
+    /// Symlink
     SymLink,
 }
 
+/// Datastructure to represent the entry in an directory
 struct DirEntry {
     private {
         string _name;
@@ -92,12 +118,26 @@ struct DirEntry {
         ulong _size = 0;
     }
 
+    /**
+     * Creates an entry from the raw name, kind and size.
+     * 
+     * Params:
+     *   name = the name of the entry; only full name allowed
+     *   kind = the kind of the entry; represnts if its a file, a directory or a symlink
+     *   size = the size of the entry; for files, this is the size of the content
+     */
     this(string name, FileKind kind, ulong size) {
         this._name = name;
         this._kind = kind;
         this._size = size;
     }
 
+    /**
+     * Creates an entry from the entry data from the dland standard library.
+     * 
+     * Params:
+     *   e = a instance of std.file.DirEntry
+     */
     this(StdDirEntry e) {
         this._name = e.name();
 
@@ -114,22 +154,47 @@ struct DirEntry {
         this._size = e.size();
     }
 
+    /**
+     * Gets the name of the entry.
+     * 
+     * Returns: the name of the entry.
+     */
     @property string name() const return scope {
         return this._name;
     }
 
+    /**
+     * Determines weather or not a entry is a directory.
+     * 
+     * Returns: true if the entry is a directory; false otherwise.
+     */
     @property bool isDir() scope {
         return this._kind == FileKind.Dir;
     }
 
+    /**
+     * Determines weather or not a entry is a regular file.
+     * 
+     * Returns: true if the entry is a regular file; false otherwise.
+     */
     @property bool isFile() scope {
         return this._kind == FileKind.File;
     }
 
+    /**
+     * Determines weather or not a entry is a symlink.
+     * 
+     * Returns: true if the entry is a symlink; false otherwise.
+     */
     @property bool isSymlink() scope {
         return this._kind == FileKind.SymLink;
     }
 
+    /**
+     * Gets the size of the entry.
+     * 
+     * Returns: the size of the entry.
+     */
     @property ulong size() scope {
         return this._size;
     }
@@ -142,10 +207,30 @@ struct DirEntry {
     // @property uint linkAttributes() scope;
 }
 
+/**
+ * Converts a std.file.DirEntry into a ninox.fs.DirEntry
+ * 
+ * Params:
+ *   e = a instance of std.file.DirEntry to be converted
+ * 
+ * Returns: a instance of ninox.fs.DirEntry with the data copied from the given std.file.DirEntry
+ */
 DirEntry toNinoxDirEntry(StdDirEntry e) {
     return DirEntry(e);
 }
 
+/**
+ * Builds a secure path by first combining the both parameters with buildNormalizedPath and
+ * then checking if its still inside the same base by checking the result with startsWith against the 'base' parameter.
+ * 
+ * Params:
+ *   base = the base directory
+ *   path = the path to append to the base
+ * 
+ * Returns: the combined & normalized path from the both parameters
+ * 
+ * Throws: NinoxFsSecurityException when the resulting path doesnt resides inside the base directory
+ */
 private string buildSecurePath(string base, string path) {
     import std.string : startsWith;
 
@@ -157,13 +242,52 @@ private string buildSecurePath(string base, string path) {
     return res;
 }
 
+/// Basic interface for an Filesystem
 interface FS {
+
+    /**
+     * Opens the file at the given name and returns a File interface allowing accessing it.
+     * 
+     * Params:
+     *   name = the name of the file to open
+     * 
+     * Returns: a file interface to accessing the file
+     */
     File open(string name);
+
+    /**
+     * Reads the content of a directory.
+     * 
+     * Params:
+     *   name = the name of the directory to read
+     * 
+     * Returns: an array of all entries in the directory
+     */
     DirEntry[] readDir(string name);
+
+    /**
+     * Reads the whole content of a file.
+     * 
+     * Params:
+     *   name = the name of the file to read
+     * 
+     * Returns: the content of the file requested as a buffer
+     */
     void[] readFile(string name);
+
+    /**
+     * Creates an sub-filesystem.
+     * 
+     * Params:
+     *   name = the name of the directory to create a sub-filesystem on
+     * 
+     * Returns: the sub-filesystem created
+     */
     FS sub(string dir);
 }
 
+/// A generic filesystem class for implementing sub-filesystems
+/// by wrapping a root filesystem and delegating all requests to them prepended with a path.
 class SubFS : FS {
     private {
         FS root;
@@ -198,6 +322,7 @@ class SubFS : FS {
     }
 }
 
+/// Filesystem onto a folder of the underlaying operating system.
 class FolderFs : FS {
     private {
         string path;
@@ -242,10 +367,19 @@ class FolderFs : FS {
     }
 }
 
+/**
+ * Gets a instance of the OS filesystem.
+ * 
+ * Params:
+ *   path = the path of the os filesystem to request
+ * 
+ * Returns: a instance of ninox.fs.FolderFs for the path requested
+ */
 FolderFs getOsFs(string path = ".") {
     return new FolderFs(path);
 }
 
+/// Entry holding informations for a file of a embedded filesystem
 struct EmbeddedFsEntry {
     void[] content;
     FileKind kind;
@@ -266,6 +400,7 @@ struct EmbeddedFsEntry {
     }
 }
 
+/// A filesystem that is embedded into the executable
 class EmbeddedFs : FS {
 
     private {
